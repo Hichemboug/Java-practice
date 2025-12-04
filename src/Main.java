1ee285cd96265c4ef7461d966c75191145645098
Hichemboug <amir.wanted96@gmail.com>
2025-12-04 22:49:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    /*Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.println("your name is : "+ name);
+        scanner.close();//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!"); */
+        /*int x = 5;
+        int y = 8;
+        System.out.println( x+ "+"+ y+"=" + (x+y) );*/
+
+        System.out.println(5+ "hello world!" +(5+3));
+
+
+
+
+    }
+}
